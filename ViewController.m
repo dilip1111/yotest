7c8d715ee97dae6d112547da3942e66b325451fd
dilip1111 <Cinemacraft@Dilips-MacBook-Pro.local>
2015-11-11 09:19:14 +0530

    From local
    From local

---
+++ ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  yotest
+//
+//  Created by Dilip on 03/11/15.
+//  Copyright © 2015 Dilip. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  yotest
+//
+//  Created by Dilip on 03/11/15.
+//  Copyright © 2015 Dilip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
